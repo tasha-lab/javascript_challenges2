db8ba6311c2d649d6faea8a3102a8792dfe45eca
Halima <natashawanjiku31@gmail.com>
2025-05-23 23:41:15 -0400

    Add formating to my code

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -60,10 +60,10 @@ findLongestWord(["Annpatrice", "Natasha", "Abdifatah", "Charity"]);
 //5. Count Properties
 function countProperties(object) {
   let properties = Object.keys(object);
-  console.log(properties.length)
+  console.log(properties.length);
 }
-countProperties({ name: "Alice", age: 25, city: "Paris" }) // returns: 3
-countProperties({ name: "Alice", age: 25, city: "Paris", lastname: "wanjiku", })
+countProperties({ name: "Alice", age: 25, city: "Paris" }); // returns: 3
+countProperties({ name: "Alice", age: 25, city: "Paris", lastname: "wanjiku" });
 
 //6. Filter by Length
 function filterByLength(arr, minlength) {
@@ -92,44 +92,47 @@ function sumEvenNumbers(arr) {
 }
 sumEvenNumbers([2, 4, 7, 5]);
 
-//8. Difference Between Sum of Even and Odd Numbers 
+//8. Difference Between Sum of Even and Odd Numbers
 
-function differenceEvenOdd(arr){
+function differenceEvenOdd(arr) {
   // let num = []
-  let evenNum = arr.filter(num => num % 2 === 0)
-  let oddNum = arr.filter(num => num % 2 !== 0)
+  let evenNum = arr.filter((num) => num % 2 === 0);
+  let oddNum = arr.filter((num) => num % 2 !== 0);
 
   let sumEven = 0;
-  for (let i =0; i<evenNum.length; i++){
-  sumEven = sumEven + evenNum[i]
+  for (let i = 0; i < evenNum.length; i++) {
+    sumEven = sumEven + evenNum[i];
   }
 
- let sumOdd=0;
-  for (let i =0; i<oddNum.length; i++){
-    sumOdd = sumOdd + oddNum[i]
+  let sumOdd = 0;
+  for (let i = 0; i < oddNum.length; i++) {
+    sumOdd = sumOdd + oddNum[i];
   }
-  let difference = sumEven - sumOdd
-  console.log(difference)
+  let difference = sumEven - sumOdd;
+  console.log(difference);
 }
 
-differenceEvenOdd([2,2,2,1,1,1])
-differenceEvenOdd([22,29,20,19,13,10])
-differenceEvenOdd([1, 2, 3, 4, 5, 6])
-
+differenceEvenOdd([2, 2, 2, 1, 1, 1]);
+differenceEvenOdd([22, 29, 20, 19, 13, 10]);
+differenceEvenOdd([1, 2, 3, 4, 5, 6]);
 
 // // 9. Count Truthy
-function countTruthy(object){
-
+function countTruthy(object) {
   let truthyCount = 0;
   let values = Object.values(object);
-  for (let i = 0; i < values.length; i++){
-    if(values[i] == "null" || values[i] == "0" || values[i] == "false" || values[i] == ""){
-      continue
+  for (let i = 0; i < values.length; i++) {
+    if (
+      values[i] == "null" ||
+      values[i] == "0" ||
+      values[i] == "false" ||
+      values[i] == ""
+    ) {
+      continue;
     } else {
       truthyCount++;
     }
   }
-  console.log(truthyCount)
+  console.log(truthyCount);
 }
 countTruthy({ a: 0, b: "hello", c: false, d: 42, e: null });
 
@@ -159,88 +162,85 @@ linearSearch([15, 10, 5, 20, 22], 5);
 linearSearch([5, 3, 1, 4], 7);
 
 //12. Reverse Linear Search
-function reverseLinearSearch(arr, num){
+function reverseLinearSearch(arr, num) {
   // let lastOccurrence= arr.length - 1
-  for (let i= arr.length -1;i>=0;i--){
-    if(arr[i]==num){
-      return i
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] == num) {
+      return i;
     }
- }
- return -1
-
+  }
+  return -1;
 }
-console.log(reverseLinearSearch([5, 3, 7, 1, 4, 7], 7))
-console.log(reverseLinearSearch([1, 2,3,4,5],10))
-console.log(reverseLinearSearch([5, 3, 7, 1, 4, 7], 0))
+console.log(reverseLinearSearch([5, 3, 7, 1, 4, 7], 7));
+console.log(reverseLinearSearch([1, 2, 3, 4, 5], 10));
+console.log(reverseLinearSearch([5, 3, 7, 1, 4, 7], 0));
 
 //13. Linear Search All Indices
-function linearSearchAll(arr, num){
-  const match=[]
-  for(let i=0;i<arr.length;i++){
-      if(arr[i]===num){
-          match.push(i)
-      }
-      
+function linearSearchAll(arr, num) {
+  const match = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      match.push(i);
+    }
   }
   return match;
 }
-console.log(linearSearchAll([5, 3, 7, 1, 4, 7], 7))
-console.log(linearSearchAll([5, 3, 7, 1, 4, 7], 0))
-console.log(linearSearchAll([5, 3, 7, 1, 44, 72], 20))
+console.log(linearSearchAll([5, 3, 7, 1, 4, 7], 7));
+console.log(linearSearchAll([5, 3, 7, 1, 4, 7], 0));
+console.log(linearSearchAll([5, 3, 7, 1, 44, 72], 20));
 
 // Challenge 14. Count Occurences
 
-function countOccurrences(arr){
+function countOccurrences(arr) {
   const objectKeyNames = new Set(arr);
-  const keyNamesArray = Array.from(objectKeyNames); 
+  const keyNamesArray = Array.from(objectKeyNames);
   let arrayValueIterations = new Object();
   let count = 0;
-  for (let i = 0; i < keyNamesArray.length; i++){
-    for (let t = 0; t < arr.length; t++){
-      if (arr[t] == keyNamesArray[i]){
+  for (let i = 0; i < keyNamesArray.length; i++) {
+    for (let t = 0; t < arr.length; t++) {
+      if (arr[t] == keyNamesArray[i]) {
         count += 1;
       }
     }
-    arrayValueIterations[keyNamesArray[i]] = count; 
+    arrayValueIterations[keyNamesArray[i]] = count;
     count = 0;
   }
-  console.log(arrayValueIterations)
+  console.log(arrayValueIterations);
 }
-countOccurrences(["apple", "banana", "apple", "orange", "banana", "apple"]) 
-countOccurrences(["ten", "five", "apple", "one", "ten", "one"]) 
+countOccurrences(["apple", "banana", "apple", "orange", "banana", "apple"]);
+countOccurrences(["ten", "five", "apple", "one", "ten", "one"]);
 
 // Challenge 15. Remove Duplicate
 
-function removeDuplicates(array){
+function removeDuplicates(array) {
   const noDuplicatesArray = Array.from(new Set(array));
-  console.log(noDuplicatesArray)
+  console.log(noDuplicatesArray);
 }
-removeDuplicates([1, 2, 3, 2, 4, 1, 5]) 
-removeDuplicates([1, 6,6,7,6,6,3,4,2,1])
+removeDuplicates([1, 2, 3, 2, 4, 1, 5]);
+removeDuplicates([1, 6, 6, 7, 6, 6, 3, 4, 2, 1]);
 
+// Challenge 16: Most Frequent
+function mostFrequent(arr) {
+  let count = {};
+  let maxCount = 0;
+  let mostFrequentValue = null;
 
-// Challenge 16. Most Frequent
-function mostFrequent(arr){
-  let mostOccuringCount = 0;
-  let mostOccuringValue;
-  const objectKeyNames = new Set(arr);
-  const keyNamesArray = Array.from(objectKeyNames);
-  let count = 0;
-  for (let i = 0; i < keyNamesArray.length; i++){
-    for (let t = 0; t < arr.length; t++){
-      if (arr[t] === keyNamesArray[i]){
-        count += 1;
-      }
-    }
-    if (count > mostOccuringCount){
-      mostOccuringValue = keyNamesArray[i];
-      mostOccuringCount = count;
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+    if (count[item] === undefined) {
+      count[item] = 1;
     } else {
-      continue
+      count[item] += 1;
     }
-    count = 0;
+
+    if (count[item] > maxCount) {
+      maxCount = count[item];
+      mostFrequentValue = item;
+    }
   }
-  console.log(mostOccuringValue)
+  return mostFrequentValue;
 }
-mostFrequent([1, 2, 2, 3, 3, 3, 4])
-mostFrequent(["apple", "banana", "apple", "orange", "banana", "apple"]) 
+console.log(mostFrequent([1, 2, 3, 2, 4, 1, 2]));
+console.log(
+  mostFrequent(["apple", "banana", "apple", "orange", "banana", "apple"]),
+);
